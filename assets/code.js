b86616d691e174a818377aa6db4fdcdfc5e713aa
arno-id <arnoboy@msn.com>
2020-07-10 10:38:16 +0200

    group level spending, saving

--- assets/code.js
+++ assets/code.js
@@ -10,9 +10,12 @@
             userSpendings: ".osszeg",
             userWeight: ".ownWeight",
             userBudget: "#havikeret",
-            modelTextarea: "#adatforras",
+            modalTextarea: "#adatforras",
             ModalSave: "#ModalSave",
             ModalID: "#adatForrasModal",
+            budgetModalTextarea: "#budgetforras",
+            budgetModalSave: "#BudgetModalSave",
+            budgetModalID: "#adatMentes",
             $tree: $(this.tree)
         },
 
@@ -58,7 +61,11 @@
                     $.Calculator.FormatNumber($(this))
                 });
                 $.Calculator.loadData();
-                $($.Calculator.settings.userBudget).data('manuallyChanged', 0);
+                $($.Calculator.settings.userBudget).data('manuallyChanged', 0); 
+
+                $($.Calculator.settings.budgetModalID).on('shown.bs.modal', function () {                 
+                    $.Calculator.LoadDataToSave();
+                });
             })
 
             $(document).on("keyup", "" + $.Calculator.settings.userBudget, function () {
@@ -69,13 +76,18 @@
             $(document).on("keyup", $.Calculator.settings.userSpendings, function (event) {
                 $.Calculator.CalcBudget();
             });
+ 
+            $(document).on("click", $.Calculator.settings.budgetModalSave, function (event) {
+                $.Calculator.RestoreSavedData();           
+            });
+            
 
             $(document).on("click", $.Calculator.settings.ModalSave, function (event) {
                 $.Calculator.InflationData = new Array();
-                $.Calculator.RawSource = $($.Calculator.settings.modelTextarea).val();
+                $.Calculator.RawSource = $($.Calculator.settings.modalTextarea).val();
                 $.Calculator.ParseRawData();
                 $.ui.fancytree.getTree($.Calculator.settings.tree).reload($.Calculator.InflationData)
-                $('#adatForrasModal').modal('hide')
+                $($.Calculator.settings.ModalID).modal('hide')
             });
 
         },
@@ -84,7 +96,35 @@
                 if (node.key.length == 3) return node;
             });
         },
-
+        this.LoadDataToSave = function(){
+            let SaveData = [];          
+            $($.Calculator.settings.userSpendings).each(function (e, v) {
+                if($(this).val() != "")
+                {
+                    let curNodeKey = $(this).data('node'); 
+                    let curspending = $.Calculator.GetPlainNuber($(this).val());
+                    SaveData.push(curNodeKey+";"+curspending);
+                }
+            }); 
+            $($.Calculator.settings.budgetModalTextarea).val(SaveData.join('\n'));
+        },
+        this.ClearSpendings = function(){
+            $($.Calculator.settings.userSpendings).each(function (e, v) {
+                $(this).val('')
+            });
+        },
+        this.RestoreSavedData = function(){
+            let data = $($.Calculator.settings.budgetModalTextarea).val().trim().split('\n');
+            $.Calculator.ClearSpendings();
+            data.forEach(x => {
+                let row = x.trim().split(';');
+                if(row.length == 2){
+                    $.Calculator.SetNodeSpending(row[0], row[1]);
+                }  
+            });
+            $.Calculator.CalcBudget();
+            $($.Calculator.settings.budgetModalID).modal('hide')
+        },     
         this.GetInflactionValue = function (key) {
             let item = $.Calculator.InflationData.find(x => x.key == key.substring(0, 1));
             if (key.length == 1) return item;
@@ -127,12 +167,10 @@
                         .find("span")
                         .text(node.data.suly);
 
-
                     $tdList
                         .eq(4)
                         .find("input")
                         .data('node', node.key);
-
                 }
             });
         },
@@ -142,7 +180,7 @@
                 url: this.settings.url
             }).done(function (data) {
                 $.Calculator.RawSource = data;
-                $($.Calculator.settings.modelTextarea).val(data);
+                $($.Calculator.settings.modalTextarea).val(data);
                 $.Calculator.ParseRawData();
             });
         },
@@ -191,16 +229,79 @@
             if ($.ui.fancytree.getTree($.Calculator.settings.tree) == null)
                 $.Calculator.initTree();
         }
-
-    this.CalcBudget = function () {
+        this.GetPlainNuber = function(v){
+            let x = parseFloat(v.replace(/[\s\ ]+/g, ""));
+            return isNaN(x) ? 0 : x;
+        }
+    
+        this.GetChildSpending = function (node){
+            let childSpending = 0; 
+            $(this.settings.userSpendings).each(function (e, v) {
+                let curNodeKey = $(this).data('node'); 
+                if( curNodeKey.substring(0, node.length) == node &&  node != curNodeKey){
+                    if($(this).val()!= ""){                     
+                        let curspending = $.Calculator.GetPlainNuber($(this).val());
+                        switch(curNodeKey.length ){                
+                            case 2:
+                                let subChildSpending = $.Calculator.GetChildSpending(curNodeKey);                              
+                                if($(this).val()!= "" &&  curspending != subChildSpending)
+                                    childSpending += curspending  - subChildSpending
+                            break;
+                            case 3:
+                                if ($(this).val() != "" &&  curNodeKey.length > node.length) 
+                                    childSpending +=  curspending;
+                            break;
+                        }
+                    }
+                }
+            });
+            return childSpending;
+        },
+        this.SetNodeSpending = function(node, spending){
+            $(this.settings.userSpendings).each(function (e, v) {
+                if ($(this).data('node') == node) {
+                    $(this).val(spending);
+                    $.Calculator.FormatNumber($(this))
+                } 
+            });
+        },
+        this.GetNodeSpending = function(node){
+            let retSpending = 0;
+            $(this.settings.userSpendings).each(function (e, v) { 
+                if ($(this).data('node') == node && $(this).val() != "") 
+                    retSpending = $.Calculator.GetPlainNuber($(this).val());              
+            });
+            return retSpending;
+        }, 
+    this.CalcBudget = function () {        
+        $($.Calculator.settings.userWeight).each(function(){$(this).text('')});
         $($.Calculator.settings.budget + " tbody").html('')
         let budget = 0.0;
         let checkedBudget = 0;
         let tempData = new Array();
+ 
+        $($(this.settings.userSpendings).get().reverse()).each(function (e, v) {                   
+            if($(this).data('node').length < 3){             
+                let childSpending =  $.Calculator.GetChildSpending($(this).data('node'));
+                let ownSpending = $.Calculator.GetNodeSpending($(this).data('node'));
+                if(isNaN(ownSpending)) ownSpending = 0;
+                if(ownSpending < childSpending){
+                    $.Calculator.SetNodeSpending($(this).data('node'), childSpending)
+                }  
+            }         
+        });
+ 
         $(this.settings.userSpendings).each(function (e, v) {
             if ($(this).val() != "") {
                 let nodeData = $.Calculator.GetInflactionValue($(this).data('node'));
-                let val = parseFloat($(this).val().replace(/[\s\ ]+/g, ""));
+                let val = $.Calculator.GetPlainNuber($(this).val());
+
+                if(nodeData.key.length < 3)
+                {
+                    let sp = $.Calculator.GetChildSpending(nodeData.key);                    
+                    val = Math.max(0, val - sp);
+                }
+
                 if ($.ui.fancytree
                     .getTree($.Calculator.settings.tree)
                     .getNodeByKey(nodeData.key).partsel)
@@ -217,6 +318,46 @@
             }
         });
 
+        //lets recalc the groups
+        //1 if a group is filled without children filled, then we are fine
+        //2 if a group is filled with children filled: - IS it smaller than the child sum? -> y -> round up group but ignore its inflation
+        //                                                                                    n -> the group should be sum() - value 
+        //3 if a group is not filled and children are, then calc the group but dont use its value.
+    /*    tempData.forEach(x => {
+            if(x.node.length == 3){ 
+                let p =[ x.node.substring(0,1), x.node.substring(0,2) ];
+                p.forEach(y => {
+                    let childSpending =  $.Calculator.GetChildSpending(y);
+                    let yData = tempData.find(q => q.node == y);
+                    let ownSpending = $.Calculator.GetNodeSpending(y);
+                    
+                    if(ownSpending < childSpending){
+                        $.Calculator.SetNodeSpending(y, childSpending)
+                    }
+
+                    if(yData == null)
+                    {    
+                        yData = {
+                            node: y,
+                            inf: $.ui.fancytree
+                            .getTree($.Calculator.settings.tree)
+                            .getNodeByKey(y).data.infla,
+                            weight: 0,
+                            checked: 0,
+                            spending: childSpending,
+                            text: ''
+                        };
+                        tempData.push(yData);                         
+                    }
+                    else {
+                        yData.spending = ownSpending < childSpending ? childSpending : ownSpending;
+                    }
+                   
+                });
+            }
+            
+        });
+*/
         let untouchedBudget = budget;
         let ubudget = $($.Calculator.settings.userBudget);
         if (ubudget.data('manuallyChanged') == 0) {
@@ -224,7 +365,7 @@
             $.Calculator.FormatNumber(ubudget)
         }
         else {
-            budget = parseFloat($($.Calculator.settings.userBudget).val().replace(/[\s\ ]+/g, ""));
+            budget = $.Calculator.GetPlainNuber($($.Calculator.settings.userBudget).val());
         }
 
         let checkedInflaHelper = 0;
